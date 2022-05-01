96aaf53182afdcd1156eb8e7d19919577bd51738
JOSÉ MARÍA GONZÁLEZ CRUZ <98995632+Jos3MGC@users.noreply.github.com>
2022-04-30 22:08:12 -0500

    ProgramaSubido

---
+++ contador-app/src/App.js
@@ -0,0 +1,39 @@
+import logo from './logo.svg';
+import './App.css';
+import React, {useState} from 'react';
+
+function App() {
+  const[inicio, sumar] = useState(0); 
+
+  const suma = () =>{
+    try {
+      sumar(inicio+1)
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Contador: {inicio}
+        </p>
+        <button onClick={suma}>Iniciar</button>
+        <br></br>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
